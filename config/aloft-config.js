05e401e69a48cec1561c0d3c235872d30ec52bd3
Stanley Sakai <stanleysakai@Stanleys-MBP.fios-router.home>
2017-06-21 17:23:31 -0400

    Login.

--- config/aloft-config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	'session': 'heybiZnatChn0St3al1ngMySh1zn!tz!',
-	'mongo': 'mongodb://127.0.0.1:27017/aloft',
-	'users': 'mongodb://127.0.0.1:27017/users',
-	'port': process.env.PORT || 4000
-}
